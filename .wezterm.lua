11669062531b8903b3b7f5a94370a41e944a1c16
Christian Engel <christian.e@schindelhauerbikes.de>
2024-09-02 15:02:59 +0200

    Update font

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,8 +4,8 @@ local wezterm = require("wezterm")
 -- this will hold the configuration
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 16
+config.font = wezterm.font("DankMono Nerd Font")
+config.font_size = 18
 
 config.enable_tab_bar = false
 
