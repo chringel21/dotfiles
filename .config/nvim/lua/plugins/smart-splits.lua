11636184cfab98f9306cdad04254fc77a7376c3a
Christian Engel <christian.e@schindelhauerbikes.de>
2024-09-06 09:50:45 +0200

    Update smart splits to acutally work with wezterm and LazyVim

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -1,15 +1,38 @@
 return {
   "mrjones2014/smart-splits.nvim",
   lazy = false,
-  init = function()
-    vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-    vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-    vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-    vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-    vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
-  end,
-  config = function()
-    local smartSplits = require("smart-splits")
-    smartSplits.setup({})
-  end,
+  keys = {
+    {
+      "<C-h>",
+      function()
+        require("smart-splits").move_cursor_left()
+      end,
+      mode = { "i", "n", "v" },
+      desc = "Move to left window",
+    },
+    {
+      "<C-j>",
+      function()
+        require("smart-splits").move_cursor_down()
+      end,
+      mode = { "i", "n", "v" },
+      desc = "Move to left window",
+    },
+    {
+      "<C-k>",
+      function()
+        require("smart-splits").move_cursor_up()
+      end,
+      mode = { "i", "n", "v" },
+      desc = "Move to left window",
+    },
+    {
+      "<C-l>",
+      function()
+        require("smart-splits").move_cursor_right()
+      end,
+      mode = { "i", "n", "v" },
+      desc = "Move to left window",
+    },
+  },
 }
