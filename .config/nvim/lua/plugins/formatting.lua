e9146f7d490db26ca346c96d7debef4882622bb0
Christian Engel <christian.e@schindelhauerbikes.de>
2024-11-01 14:18:38 +0100

    Add prettier

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -2,6 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      js = { "prettier" },
       vue = { "prettier" },
       css = { "prettier" },
       graphql = { "prettier" },
