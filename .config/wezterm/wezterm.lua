173df5b7188486337a4dc474210c3abfe3dd7a33
Christian Engel <christian.e@schindelhauerbikes.de>
2025-05-13 13:35:11 +0200

    Move wezterm config to sub folder

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,171 @@
+-- Pull the wezterm API
+local wezterm = require("wezterm")
+local utf8 = require("utf8")
+
+-- Session manager
+-- https://github.com/danielcopper/wezterm-session-manager.git
+local session_manager = require("wezterm-session-manager/session-manager")
+wezterm.on("save_session", function(window)
+	session_manager.save_state(window)
+end)
+wezterm.on("load_session", function(window)
+	session_manager.load_state(window)
+end)
+wezterm.on("restore_session", function(window)
+	session_manager.restore_state(window)
+end)
+
+-- Workspaces
+wezterm.on("update-right-status", function(window, pane)
+	window:set_right_status(window:active_workspace())
+end)
+
+-- tab bar status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f680)
+		SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
+
+-- pane switching with nvim
+local function is_vim(pane)
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = "CTRL" },
+				}, pane)
+			else
+				win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+			end
+		end),
+	}
+end
+
+-- this will hold the configuration
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("DankMono Nerd Font")
+config.font_size = 18
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+config.send_composed_key_when_left_alt_is_pressed = true
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+-- key bindings
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- toggle pane fullscreen
+	{
+		mods = "LEADER",
+		key = "m",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	-- close pane
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	-- rotating panes
+	{
+		key = "b",
+		mods = "CTRL",
+		action = wezterm.action.RotatePanes("CounterClockwise"),
+	},
+	{ key = "n", mods = "CTRL", action = wezterm.action.RotatePanes("Clockwise") },
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+
+	-- session manager
+	{ key = "S", mods = "LEADER", action = wezterm.action({ EmitEvent = "save_session" }) },
+	{ key = "L", mods = "LEADER", action = wezterm.action({ EmitEvent = "load_session" }) },
+	{ key = "R", mods = "LEADER", action = wezterm.action({ EmitEvent = "restore_session" }) },
+
+	-- workspaces
+	-- Switch to the default workspace
+	{
+		key = "y",
+		mods = "LEADER",
+		action = wezterm.action.SwitchToWorkspace({
+			name = "default",
+		}),
+	},
+	-- Create a new workspace with a random name and switch to it
+	{ key = "i", mods = "LEADER", action = wezterm.action.SwitchToWorkspace },
+	-- Show the launcher in fuzzy selection mode and have it list all workspaces
+	-- and allow activating one.
+	{
+		key = "9",
+		mods = "LEADER",
+		action = wezterm.action.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
+}
+
+return config
