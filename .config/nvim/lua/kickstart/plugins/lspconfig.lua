011ee0c623ab3a8663191ecc13084e864d4a5ef4
Christian Engel <christian.e@schindelhauerbikes.de>
2025-05-23 12:55:08 +0200

    Change volar to vue_ls

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -236,19 +236,19 @@ return {
             },
           },
         },
+        vue_ls = {},
         ts_ls = {
           init_options = {
             plugins = {
               {
                 name = '@vue/typescript-plugin',
                 location = vue_language_server_path,
-                languages = { 'vue' },
+                languages = { 'javascript', 'typescript', 'vue' },
               },
             },
           },
           filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
         },
-        volar = {},
         eslint_d = {},
         prettier = {},
       }
