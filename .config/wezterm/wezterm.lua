8995060207d2ac74f5e444ec7173cb35074ab7ec
Christian Engel <christian.e@schindelhauerbikes.de>
2025-08-14 13:17:14 +0200

    Change wezterm tab bar config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,29 +7,6 @@ wezterm.on("update-right-status", function(window, pane)
 	window:set_right_status(window:active_workspace())
 end)
 
--- tab bar status
-wezterm.on("update-right-status", function(window, _)
-	local SOLID_LEFT_ARROW = ""
-	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-	local prefix = ""
-
-	if window:leader_is_active() then
-		prefix = " " .. utf8.char(0x1f680)
-		SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-	end
-
-	if window:active_tab():tab_id() ~= 0 then
-		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-	end
-
-	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#b7bdf8" } },
-		{ Text = prefix },
-		ARROW_FOREGROUND,
-		{ Text = SOLID_LEFT_ARROW },
-	}))
-end)
-
 -- pane switching with nvim
 local function is_vim(pane)
 	return pane:get_user_vars().IS_NVIM == "true"
@@ -79,10 +56,70 @@ config.macos_window_background_blur = 10
 
 config.send_composed_key_when_left_alt_is_pressed = true
 
--- tab bar
+-- tab bar config
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = "" }
+	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+	return current_dir == HOME_DIR and "." or string.gsub(current_dir.file_path, "(.*[/\\])(.*)", "%2")
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local has_unseen_output = false
+	if not tab.is_active then
+		for _, pane in ipairs(tab.panes) do
+			if pane.has_unseen_output then
+				has_unseen_output = true
+				break
+			end
+		end
+	end
+
+	local cwd = wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		{ Text = get_current_working_dir(tab) },
+	})
+
+	local title = string.format(" [%s] %s ", tab.tab_index + 1, cwd)
+
+	if has_unseen_output then
+		return {
+			{ Foreground = { Color = "#8866bb" } },
+			{ Text = title },
+		}
+	end
+
+	return {
+		{ Text = title },
+	}
+end)
+
+-- tab bar status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f680)
+		SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
 
 -- key bindings
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
