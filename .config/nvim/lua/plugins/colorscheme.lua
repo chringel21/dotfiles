77b1cb65845ebfa1212274fe48c534da9ed2ace5
Christian Engel <christian.e@schindelhauerbikes.de>
2026-04-10 11:14:53 +0200

    Update colorscheme config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
